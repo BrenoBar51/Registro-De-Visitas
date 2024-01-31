a54f2a1ebff4a505cdf5b9fd6c2feb2332a89283
BrenoBar51 <brenobarcelos9@gmail.com>
2024-01-31 09:53:34 -0300

    Update back-end

--- back-end/index.js
+++ back-end/index.js
@@ -1,10 +1,31 @@
-const router = require('express').Router();
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
+const express = require('express');
+const app = express();
+const cors = require('cors');
 const port = 8000;
 
-router.use('/api-docs', swaggerUi.serve);
-router.get('/api-docs', swaggerUi.setup(swaggerDocument));
+const brandRoutes = require('./routes/brandRoutes');
+const categoriesRoutes = require('./routes/categoriesRoutes');
+const gendersRoutes = require('./routes/genderRoutes');
+const userRoutes = require('./routes/userRoutes');
+const reviewsRoutes = require('./routes/reviewsRoutes');
+
+
+app.use(express.json());//middlewares
+app.use(cors());//middlewares
+
+app.get('/', (request, response) => {
+    response.send('Bem-vindo à API da Digital Store');
+});
+
+app.use('/user', userRoutes);
+app.use('/marcas', brandRoutes);
+app.use('/categories', categoriesRoutes);
+app.use('/generos', gendersRoutes);
+app.use('/reviews', reviewsRoutes);
+
+app.all('*', (req, res) =>{
+    res.status(404).send('Rota não encontrada');
+});
 
 app.listen(port, () => {
     console.log(`Servidor de pé na url: http://localhost:${port}`);
