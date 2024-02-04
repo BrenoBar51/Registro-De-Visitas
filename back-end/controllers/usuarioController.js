072db72341823d326d916313d06ffa62e4558b08
BrenoBar51 <brenobarcelos9@gmail.com>
2024-02-04 13:27:44 -0300

    controllers back-end

--- back-end/controllers/usuarioController.js
+++ back-end/controllers/usuarioController.js
@@ -1,3 +1,44 @@
+const DB = require('../database/index');
+const tabela = 'usuario';
+
+async function listar(){
+    return await DB.execute(`SELECT * FROM ${tabela};`);
+}
+
+async function listarUM(id){
+    return await DB.execute(`SELECT * FROM ${tabela} WHERE brand_id = ${id};`);
+}
+
+async function cadastrar(data){
+    try{
+        const linha = await DB.execute(`INSERT INTO ${tabela} (usuario_email) VALUES ('${data.usuario_email}');`);
+        return listarUM(linha.insertId);
+    } catch(error){
+        return{
+            type: 'error',
+            message: `Erro ao inserir dados: ${error.message}`
+        }
+    }
+};
+
+async function deletar(id){
+    try{
+        if(!id){
+            throw new Error('usuario_id é obrigatório');
+        }
+        await DB.execute(`DELETE FROM ${tabela} WHERE brand_id = ${id}`);
+        return {
+            type: 'error',
+            message: 'Dados apagados com sucesso!'
+        }
+    } catch (error){
+        return{
+            type: 'error',
+            message: `Deu ruim: ${error}`
+        }
+    }
+}
+
 async function logar(data){
     try{
         if(!data.email){
@@ -20,5 +61,9 @@ async function logar(data){
 }
 
 module.exports = {
-    logar
+    logar,
+    listar,
+    listarUM,
+    cadastrar,
+    deletar
 }
